d06488af5b7f3dbb618aaddae363e43a1bb7103f
minhazf456 <minhazf@gmail.com>
2020-01-19 21:24:22 -0800

    GUI and inheritance and Polymorphism

---
+++ MemoryModels/src/IntAssignmentTester.java
@@ -0,0 +1,10 @@
+
+public class IntAssignmentTester {
+	public static void main (String[] args) {
+		int var1 =17;
+		int var2= var1 + 1;
+		var1 = var2 +1;
+		System.out.print("var1: " + var1 + " var2: " + var2);
+	}
+
+}
